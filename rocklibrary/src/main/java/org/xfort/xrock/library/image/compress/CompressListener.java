a77bd484096cf0e05bb85f6566c9b35c86fffe60
zhx <benpaxiu@gmail.com>
2020-03-09 21:58:28 +0800

    升级

--- rocklibrary/src/main/java/org/xfort/xrock/library/image/compress/CompressListener.java
+++
@@ -1,9 +0,0 @@
-package org.xfort.xrock.library.image.compress;
-
-/**
- * Created by tongying on 18-3-23.
- */
-
-public interface CompressListener {
-    void onFinished(String srcFilePath, String outFilePath, Exception e);
-}
